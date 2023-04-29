056e6e15433a53f9c975627d227ffda7f2837424
Ariel Mattos <arielmattosnev@gmail.com>
2023-04-29 17:07:56 -0300

    put the project on github

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>PokeNext</title>
+      </Head>
+      <div className="text-xl">
+        <p>Meu projeto da pokedex em nextjs</p>
+      </div>
+    </>
+  );
+}
