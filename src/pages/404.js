339e4ac4eb79064333bf507ba64caefa11cc7cf9
Ariel Mattos <arielmattosnev@gmail.com>
2023-04-29 21:32:44 -0300

    modicate the page error 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 function NotFound() {
   return (
     <div className="flex items-center justify-center flex-col h-screen text-red-700">
-      <h1 className="text-4xl">404</h1>
+      <h1 className="text-4xl">Error 404</h1>
       <p className="text-2xl">Página não encontrada</p>
     </div>
   );
