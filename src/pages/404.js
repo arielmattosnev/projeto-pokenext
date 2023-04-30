ebd43a7c5462c2b16eccb40e7d30c140509dbfb2
Ariel Mattos <arielmattosnev@gmail.com>
2023-04-29 21:30:14 -0300

    insert error 404 page in the project

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <div className="flex items-center justify-center flex-col h-screen text-red-700">
+      <h1 className="text-4xl">404</h1>
+      <p className="text-2xl">Página não encontrada</p>
+    </div>
+  );
+}
+
+export default NotFound;
