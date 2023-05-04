24c9ee9601225770d481ad80b57967d224789d32
Ariel Mattos <arielmattosnev@gmail.com>
2023-05-04 13:30:08 -0300

    feat: update the css of pokemon card

--- src/pages/components/Card.jsx
+++ src/pages/components/Card.jsx
@@ -3,17 +3,20 @@ import Link from "next/link";
 
 const Card = ({ pokemon }) => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center bg-red-800/80 text-white p-2 mb-3 mr-0.5 border-2 border-red-500 rounded-xl poke_card">
       <Image
         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png `}
-        width="120"
-        height="120"
+        width="140"
+        height="140"
         alt="pokemon image"
       />
-      <p className="text-lg">#{pokemon.id}</p>
+      <p className="text-lg bg-red-700 p-1 rounded-md">#{pokemon.id}</p>
       <h3 className="text-lg font-bold uppercase">{pokemon.name}</h3>
-      <Link href={`/pokemon/${pokemon.id}`}>
-        <span className="underline text-blue-600 hover:text-blue-900">Detalhes</span>
+      <Link
+        href={`/pokemon/${pokemon.id}`}
+        className="bg-white text-black my-2 p-1 rounded-md hover:bg-gray-300"
+      >
+        <span>Detalhes do Pokémon</span>
       </Link>
     </div>
   );
